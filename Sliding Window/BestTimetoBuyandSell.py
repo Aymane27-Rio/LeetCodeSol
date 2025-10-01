ec8636c67a1e5bb03229838914b4c696b690f703
Aymane <147632834+Aymane27-Rio@users.noreply.github.com>
2025-10-01 22:35:56 +0100

    new folder: sliding window

---
+++ Sliding Window/BestTimetoBuyandSell.py
@@ -0,0 +1,53 @@
+#  Link: https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description
+
+
+# You are given an array prices where prices[i] is the price of a given stock on the ith day.
+
+# You want to maximize your profit by choosing a single day to buy one stock and choosing a different day in the future to sell that stock.
+
+# Return the maximum profit you can achieve from this transaction. If you cannot achieve any profit, return 0.
+
+ 
+
+# Example 1:
+
+# Input: prices = [7,1,5,3,6,4]
+# Output: 5
+# Explanation: Buy on day 2 (price = 1) and sell on day 5 (price = 6), profit = 6-1 = 5.
+# Note that buying on day 2 and selling on day 1 is not allowed because you must buy before you sell.
+
+
+
+
+#  Passes 154/212 test cases
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        a = min(prices)
+        prices = prices[prices.index(a):len(prices)]
+        b = max(prices)
+        return b - a
+    
+
+
+#  Passes all testcases
+
+class Solution(object):
+    def maxProfit(self, prices):
+        """
+        :type prices: List[int]
+        :rtype: int
+        """
+        if not prices:
+            return 0
+        min0 = prices[0]
+        max0 = 0
+        for price in prices:
+            min0 = min(price, min0)
+            max0 = max(max0, price - min0)
+        return max0
+        
